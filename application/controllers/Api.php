6a1fe3abfa4c4e33db92a94ad9bc3bfd0266d71d
lucky.li <lucky9303@163.com>
2017-10-13 16:49:58 +0800

    menu test

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -10,8 +10,6 @@ class Api extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library("Makemenu");
-        $this->makemenu->dolist();
     }
 
     public function index()
@@ -23,6 +21,8 @@ class Api extends CI_Controller
         }else{
             $this->responseMsg();
         }
+        $this->load->library("Makemenu");
+        $this->makemenu->dolist();
     }
     //定义验证签名方法
     public function valid($echoStr)
